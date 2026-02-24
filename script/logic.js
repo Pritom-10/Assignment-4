84d71390e694801122dc185762304fabe596fa5b
Pritom-10 <pritomshiro@gmail.com>
2026-02-24 19:05:12 +0600

    add main container in js

--- script/logic.js
+++ script/logic.js
@@ -24,3 +24,70 @@ function calculateCount() {
   interviewCount.innerText = interviewList.length;
   rejectedCount.innerText = rejectedList.length;
 }
+
+function toggle(id) {
+  allButton.classList.add("btn-soft");
+  interButton.classList.add("btn-soft");
+  rejectButton.classList.add("btn-soft");
+
+  allButton.classList.remove("btn-info");
+  interButton.classList.add("btn-info");
+  rejectButton.classList.add("btn-info");
+
+  const selected = document.getElementById(id);
+  currentStatus = id;
+  selected.classList.remove("btn-soft");
+  selected.classList.add("btn-info");
+
+  if (id == "int_btn") {
+    allcards.classList.add("hidden");
+    generate.classList.remove("hidden");
+    addDiv();
+  } else if (id == "all_btn") {
+    allcards.classList.remove("hidden");
+    generate.classList.add("hidden");
+    document.getElementById("empthy").style.display = "none";
+  } else if (id == "rej_btn") {
+    allcards.classList.add("hidden");
+    generate.classList.remove("hidden");
+    rejaddDiv();
+  }
+}
+
+mainContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("int_btn")) {
+    const parentNode = e.target.parentNode.parentNode.parentNode;
+    const jobName = parentNode.querySelector(".card-title").innerText;
+    const status = parentNode.querySelector(".notApplied");
+    const positions = parentNode.querySelector(".position").innerText;
+    const salery = parentNode.querySelector(".salery").innerText;
+    const describe = parentNode.querySelector(".describe").innerText;
+
+    status.innerText = "Interviwed";
+    status.classList.add("btn-success");
+
+    const cardInfo = {
+      jobName: jobName.trim(),
+      positions,
+      salery,
+      status,
+      describe,
+    };
+
+    const jobExist = interviewList.find(
+      (item) => item.jobName == cardInfo.jobName,
+    );
+    if (!jobExist) {
+      interviewList.push(cardInfo);
+    }
+
+    rejectedList = rejectedList.filter(
+      (item) => item.jobName != cardInfo.jobName,
+    );
+
+    calculateCount();
+
+    if (currentStatus == "rej_btn") {
+      rejaddDiv();
+    }
+  }
